b48a8ce8085700d3945542bf260b7441c9bacebc
KReguieg <Khaled.Reguieg@gmail.com>
2025-04-14 18:46:23 +0200

    fixed typo in intro log

--- main.cs
+++ main.cs
@@ -1,7 +1,9 @@
 public static string name = "Khaled"
 public static string lastname = "Reguieg"
 
+public static string age = "34"
+
 static void Main(string[] args)
 {
-    Console.WriteLine($"Hello my name is {name} {lastname}.");
+    Console.WriteLine($"Hello my name is {name} {lastname}, I'm {age} years old.");
 }
