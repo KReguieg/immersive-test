c629a3415914fccb8cdeead04cedb4dad6da245e
KReguieg <Khaled.Reguieg@gmail.com>
2025-04-14 18:51:23 +0200

    Added fav anime in bio prompt

--- main.cs
+++ main.cs
@@ -4,4 +4,7 @@ public static string lastname = "Reguieg"
 static void Main(string[] args)
 {
     Console.WriteLine($"Hello my name is {name} {lastname}.");
+    Console.WriteLine("Enter your favorite anime:");
+    string favAnime = Console.ReadLine();
+    Console.WriteLine($"Cool! I love {favAnime}! <3")
 }
