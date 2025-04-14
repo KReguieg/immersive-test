84c3a5aa976fa29b446f8d3633ec8f68491fa688
KReguieg <Khaled.Reguieg@gmail.com>
2025-04-14 19:08:17 +0200

    Added color and new line

--- main.cs
+++ main.cs
@@ -8,5 +8,9 @@ static void Main(string[] args)
     Console.WriteLine($"Hello my name is {name} {lastname}, I'm {age} years old.");
     Console.WriteLine("Enter your favorite anime:");
     string favAnime = Console.ReadLine();
-    Console.WriteLine($"Cool! I love {favAnime}! <3")
+    Console.WriteLine($"Cool! I love {favAnime}! <3\n")
+
+    Console.WriteLine("Enter your favorite color:");
+    string favColor = Console.ReadLine();
+    Console.WriteLine($"Cool! {favColor} is an awesome color! <3")
 }
