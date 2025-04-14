5bd490911d04c2dce2d3d45c58aa2a459575c07b
KReguieg <Khaled.Reguieg@gmail.com>
2025-04-14 18:41:46 +0200

    Added lastname to main

--- main.cs
+++ main.cs
@@ -1,6 +1,7 @@
 public static string name = "Khaled"
+public static string lastname = "Reguieg"
 
 static void Main(string[] args)
 {
-    Console.WriteLine($"Hello my name is {name}.");
+    Console.WriteLine($"Hello my name is {name} {lastname}.");
 }
